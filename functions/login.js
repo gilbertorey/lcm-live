2bd68ef816aefc32796bed7b63a3ee880271672e
gilbertorey <gilberto@brhyt.com>
2023-03-16 02:19:43 -0500

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -8,7 +8,7 @@ exports.handler = async function (event, context) {
   const endpoint = `${process.env.URL}/.netlify/identity/token`;
   const data = querystring.stringify({
     grant_type: 'password',
-    username: 'email@example.com',
+    username: 'email@lacasamelaza.com',
     password: password,
   });
   const options = {
