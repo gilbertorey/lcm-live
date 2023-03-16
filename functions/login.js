168f6bcaba71608b2685adaa3d9458a81c74acea
gilbertorey <gilberto@brhyt.com>
2023-03-16 05:10:27 -0500

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -26,7 +26,7 @@ exports.handler = async function (event, context) {
       headers: {
         'Set-Cookie': `nf_jwt=${access_token}; Path=/; HttpOnly; Secure`,
         'Cache-Control': 'no-cache',
-        Location: redirect || '/ps/',
+        Location: redirect || '/pro/',
       },
     };
   } catch (error) {
